e87a6ff3e9979812e2cc1db41511277aea12462b
Bomin <119003827+Bbomcd@users.noreply.github.com>
2024-08-17 19:52:40 +0900

    Add files via upload

---
+++ testdd.py
@@ -0,0 +1,8 @@
+n1 = int(input("숫자1 입력: "))
+n2 = int(input("숫자2 입력: "))
+n3 = int(input("숫자3 입력: "))
+add = n1 + n2 + n3
+mul = n1 * n2 * n3
+
+print(n1, "+", n2, "+", n3, "=", add)
+print(n1, "*", n2, "*", n3, "=", mul)
